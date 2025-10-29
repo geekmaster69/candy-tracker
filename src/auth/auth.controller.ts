1cb2d9f1efd338b16d567a469bfce5b174e2e0b0
Emmanuel Morales <emmanuelmosa@gmail.com>
2025-10-28 21:37:39 -0600

    argon fixed

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
-import { User } from './entities/user.entity';
 
 
 
--- src/config/plugins/atgon_2.plugin.ts
+++ src/config/plugins/atgon_2.plugin.ts
@@ -1,13 +1,14 @@
 
-import argon2 from 'argon2';
+import * as argon2 from 'argon2';
 
 
 export class Argon2Plugin {
     static async hash(password: string): Promise<string> {
-        return argon2.hash(password);
+
+        return await argon2.hash(password);
     }
 
     static async verify(hazingPassword: string, password: string): Promise<boolean> {
-        return argon2.verify(hazingPassword, password);
+        return await argon2.verify(hazingPassword, password);
     }
 }
